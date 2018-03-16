1bf2aaffb04d67063fbcc0328157fe0bee0f738b
Fahad Alrefae <fahad.alrefae95@gmail.com>
2018-03-15 22:09:53 -0700

    convolution function updated

--- imagetest.py
+++ imagetest.py
@@ -60,45 +60,25 @@ def normalize():
     return iar
 #print(normalize(resize())
 
+
 def convolve(image, kernel):
-	'''# grab the spatial dimensions of the image, along with
-	# the spatial dimensions of the kernel'''
-	(iH, iW) = image.shape[:2]
-	(kH, kW) = kernel.shape[:2]
 
-	'''# allocate memory for the output image, taking care to
-	# "pad" the borders of the input image so the spatial
-	# size (i.e., width and height) are not reduced'''
-	pad = (kW - 1) / 2
-	image = cv2.copyMakeBorder(image, pad, pad, pad, pad,
-		cv2.BORDER_REPLICATE)
-	output = np.zeros((iH, iW), dtype="float32")
-    # loop over the input image "sliding" the kernel across
-    # each (x, y)-coordinate from left-to-right and top to
-	# bottom'''
+    output = np.zeros((size, size), dtype="float32")
+    Sum1 = 0.0
+    Sum2 = 0.0
+    j = 1
+    i = 1
 
-	for y in np.arange(pad, iH + pad):
-		for x in np.arange(pad, iW + pad):
-			'''# extract the ROI of the image by extracting the
-			# *center* region of the current (x, y)-coordinates
-			# dimensions'''
-			roi = image[y - pad:y + pad - 1, x - pad:x + pad - 1]
-
-			'''# perform the actual convolution by taking the
-			# element-wise multiplicate between the ROI and
-			# the kernel, then summing the matrix'''
-			k = (roi * kernel).sum()
-
-			'''# store the convolved value in the output (x,y)-
-			# coordinate of the output image'''
-			output[y - pad, x - pad] = k
-    # rescale the output image to be in the range [0, 255]
-	output = rescale_intensity(output, in_range=(0, 255))
-	output = (output * 255).astype("uint8")
-
-	# return the output image
-	return output
-
+    r = 4
+    for k in range(r):
+        for m in range(r):
+            for n in range(r):
+                Sum1 = Sum1 + (float(image[(i + m - 1), (j + n - 1)]) * kernel[k][m][n])
+            Sum2 = Sum2 + Sum1
+            output[i , j] = Sum2
+            i +=1
+            j +=1
+    return output
 
 
 
